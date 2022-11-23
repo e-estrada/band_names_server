d94bdb1f1d3f03747b990e6d611d7e093d83dbe9
Erick Estrada <eof105@gmail.com>
2022-11-23 15:39:59 -0600

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,12 +1,41 @@
 
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Héroes del Silencio'));
+bands.addBand(new Band('Metallica'));
+
+
 //Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado...');
+
+    client.emit('active-bands', bands.getBands());
     
     client.on('mensaje', (data) => { 
         console.log(data);
         io.emit('mensaje', {admin: 'Nuevo mensaje'});
+    });
+
+    client.on('vote-band', (data) => {
+        bands.voteBand(data.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (data) => {
+        const newBand = new Band(data.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (data) => {
+        bands.deleteBand(data.id);
+        io.emit('active-bands', bands.getBands());
     });
 
     client.on('disconnect', () => { 
